1633b6dbad8705a4a7b148bac3e98025c3d3772a
Chris Watson <cawatson1993@gmail.com>
2025-11-08 13:20:29 -0700

    test(storage): add memory storage specs

---
+++ spec/shrine/storage/memory_spec.cr
@@ -0,0 +1,34 @@
+require "../../spec_helper"
+
+describe Shrine::Storage::Memory do
+  it "uploads, reads, checks existence, and deletes" do
+    storage = Shrine::Storage::Memory.new
+
+    storage.upload(IO::Memory.new("data"), "id")
+    storage.exists?("id").should be_true
+
+    io = storage.open("id")
+    io.gets_to_end.should eq "data"
+
+    storage.url("id").should eq "memory://id"
+
+    storage.delete("id")
+    storage.exists?("id").should be_false
+  end
+
+  it "delete_prefixed and clear! work" do
+    storage = Shrine::Storage::Memory.new
+
+    storage.upload(IO::Memory.new("a"), "foo/a")
+    storage.upload(IO::Memory.new("b"), "foo/b")
+    storage.upload(IO::Memory.new("c"), "bar/c")
+
+    storage.delete_prefixed("foo")
+    storage.exists?("foo/a").should be_false
+    storage.exists?("foo/b").should be_false
+    storage.exists?("bar/c").should be_true
+
+    storage.clear!
+    storage.exists?("bar/c").should be_false
+  end
+end
