eee1057044af89cc41fda884f84344325bb7c421
Chris Watson <cawatson1993@gmail.com>
2025-11-08 14:09:37 -0700

    fix(s3): generate correct URLs for custom endpoints

--- spec/shrine/storage/s3_spec.cr
+++ spec/shrine/storage/s3_spec.cr
@@ -15,11 +15,57 @@ describe Shrine::Storage::S3 do
     client = Awscr::S3::Client.new("key", "secret", "us-east-2")
 
     storage = Shrine::Storage::S3.new("bucket", client)
-    storage.url("foo.jpg").should contain("bucket")
-    storage.url("foo.jpg").should contain("foo.jpg")
+    url = storage.url("foo.jpg")
+    url.should contain("bucket")
+    url.should contain("foo.jpg")
 
     prefixed = Shrine::Storage::S3.new("bucket", client, "prefix")
-    prefixed.url("foo.jpg").should contain("bucket")
-    prefixed.url("foo.jpg").should contain("prefix/foo.jpg")
+    prefixed_url = prefixed.url("foo.jpg")
+    prefixed_url.should contain("bucket")
+    prefixed_url.should contain("prefix/foo.jpg")
+  end
+
+  it "uses custom endpoint host for URLs" do
+    client = Awscr::S3::Client.new("key", "secret", "us-east-2", endpoint: "http://localhost:9000")
+    storage = Shrine::Storage::S3.new("bucket", client)
+
+    url = storage.url("foo.jpg")
+    url.should contain("localhost:9000")
+    url.should contain("bucket")
+    url.should contain("foo.jpg")
+  end
+
+  it "uses custom endpoint with non-default port" do
+    client = Awscr::S3::Client.new("key", "secret", "us-east-2", endpoint: "http://127.0.0.1:9000")
+    storage = Shrine::Storage::S3.new("bucket", client)
+
+    url = storage.url("foo.jpg")
+    url.should contain("127.0.0.1:9000")
+    url.should contain("bucket")
+    url.should contain("foo.jpg")
+  end
+
+  it "allows overriding host via :host option" do
+    client = Awscr::S3::Client.new("key", "secret", "us-east-2")
+    storage = Shrine::Storage::S3.new("bucket", client)
+
+    url = storage.url("foo.jpg", host: "cdn.example.com")
+    url.should contain("cdn.example.com")
+    url.should contain("bucket")
+    url.should contain("foo.jpg")
+  end
+
+  it "accepts different HTTP methods without raising" do
+    client = Awscr::S3::Client.new("key", "secret", "us-east-2")
+    storage = Shrine::Storage::S3.new("bucket", client)
+
+    # These should all succeed and return a URL string
+    storage.url("foo-get.jpg", method: :get).should be_a(String)
+    storage.url("foo-put.jpg", method: :put).should be_a(String)
+
+    # Unknown/unsupported methods should gracefully fall back to :get
+    storage.url("foo-head.jpg", method: :head).should be_a(String)
+    storage.url("foo-post.jpg", method: :post).should be_a(String)
+    storage.url("foo-unknown.jpg", method: :unknown).should be_a(String)
   end
 end
