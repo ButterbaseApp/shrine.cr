3ad5fe303120e72d02e2c1b22953f6e02b84973d
Chris Watson <cawatson1993@gmail.com>
2026-01-16 23:35:58 -0700

    fix(s3): return IO from open method and rewind before returning
    The open method was not returning the IO object, resulting in empty
    file content when reading from S3. Also use IO.copy for proper
    streaming and rewind to reset position for reading.

--- src/shrine/storage/s3.cr
+++ src/shrine/storage/s3.cr
@@ -65,11 +65,10 @@ class Shrine
       def open(id : String, **options) : IO
         io = IO::Memory.new
         client.get_object(bucket, object_key(id)) do |obj|
-          io << obj
+          IO.copy(obj, io)
         end
-
-        # io
-        # client.get_object(bucket, object_key(id)).body_io
+        io.rewind
+        io
       end
 
       # Returns the presigned URL to the file.
